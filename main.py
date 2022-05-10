7279da48904004c9599b895e88493bb206cb9e86
todd <todd.develop@gmail.com>
2022-05-10 09:16:41 +0300

    qwe

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def get_web_driver_options(proxy: dict = None) -> any:
     options.add_argument("--headless")
     # options.headless = HEADLESS_MODE
 
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference('dom.webdriver.enabled', False)
-    profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0")
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_preference('dom.webdriver.enabled', False)
+    # profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0")
     root_path = os.getcwd()
 
     driver = webdriver.Firefox(
         executable_path=f"{root_path}/geckodriver",
         options=options,
         # seleniumwire_options=proxy,
-        firefox_profile=profile,
+        # firefox_profile=profile,
     )
     driver.set_page_load_timeout(3600 * 2 * 2)
     return driver
